17150a97a38e1ae7b31ac5cf2f7a68ae208fd92b
Ben Chapman <ben.chapman@iress.com>
2022-12-02 00:36:48 +0000

    Add solution for Day 1

---
+++ Day 1/Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿var descendingComparer = Comparer<int>.Create((a, b) => b.CompareTo(a));
+var sortedCalories = new SortedSet<int>(descendingComparer);
+var calorieCount = 0;
+
+var foodItems = File.ReadAllLines("input.txt");
+
+foreach (var item in foodItems)
+{
+    if (item.Length > 0)
+    {
+        calorieCount += int.Parse(item);
+        continue;
+    }
+    
+    sortedCalories.Add(calorieCount);
+    calorieCount = 0;
+}
+
+Console.WriteLine(sortedCalories.Max());
+Console.WriteLine(sortedCalories.Take(3).Sum());
+Console.ReadLine();
